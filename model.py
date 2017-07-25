2bccc56b0305d6919f6db3dc47444a6fc08dda4a
Howard Butler <howard.butler@intel.com>
2017-07-25 14:52:20 -0700

    clean up for final submission

--- model.py
+++ model.py
@@ -6,37 +6,6 @@ import matplotlib
 
 from random import shuffle
 
-def compare_images(left_image, right_image):
-    print(type(left_image))
-    print(left_image.shape)
-    print(type(right_image))
-    print(right_image.shape)
-    
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
-    #ax1.imshow(cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB))
-    ax1.imshow(left_image)
-    ax1.set_title('Shape '+ str(left_image.shape),
-                  fontsize=50)
-    #ax2.imshow(cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB))
-    ax2.imshow(right_image)
-    #cv2.imshow('image', right_image)
-    ax2.set_title('Shape '+ str(right_image.shape)
-                  , fontsize=50)
-    plt.show()
-
-def compare_cv_images(left_image, right_image):
-
-    resized_image = cv2.resize(right_image, (320, 160))
-    print(left_image.shape)
-    print(resized_image.shape)
-    
-    final_frame = cv2.hconcat((left_image, resized_image))
-    cv2.imshow('lena', final_frame)
-    #cv2.imshow('image',left_image)
-    #cv2.imshow('image', right_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 def bgr_to_rgb(image):
     b,g,r = cv2.split(image)
     return cv2.merge([r,g,b])
@@ -190,11 +159,7 @@ def augment_data(center_images, left_images, right_images, measurements):
     center_images, left_images, right_images, measurements = reduce_centered_images(0.9, center_images, left_images, right_images, measurements)
     center_images,left_images, right_images,  measurements = flip_images(center_images, left_images, right_images,  measurements)   
     center_images, measurements = add_side_cameras(steering_correction, 0.2, center_images, left_images, right_images, measurements)
-
     center_images, measurements = reduce_centered_side_images(0.9, center_images, measurements, 0.0)
-    #center_images, measurements = reduce_centered_side_images(0.5, center_images, measurements, steering_correction)
-    #center_images, measurements = reduce_centered_side_images(0.5, center_images, measurements, -steering_correction)
- 
     
     return center_images, left_images, right_images, measurements
 
@@ -212,8 +177,8 @@ def plot_measurement_histogram(measurements):
     plt.title('Steering Angle Historgram')
     plt.ylabel('number of measurements')
     plt.xlabel('angle')
-    plt.show()
-    #plt.savefig('images/steering_hist.png')
+    #plt.show()
+    plt.savefig('images/steering_hist.png')
 
 
 from keras.models import Sequential, Model, load_model
@@ -224,48 +189,11 @@ from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 
-def visualize_model(X_train, models):
-    model = load_model('model.h5')
-    
-    crop_layer = model.layers[2].output
-    output_fn = K.function([model.layers[0].input], [crop_layer])
-
-    input_image = X_train[0:1:,:,:]
-    print(input_image.shape)
-    
-
-    output_image = output_fn([input_image])
-    print(output_image[0].shape)
-
-    output_image = np.rollaxis(np.rollaxis(output_image[0], 3, 1), 3, 1)
-    print(output_image.shape)
-
-
-
-    print(output_image.shape)
-
-    fig=plt.figure(figsize=(8,8))
-    for i  in range(31):
-        ax = fig.add_subplot(6,6,i+1)
-        ax.imshow(output_image[0,:,:,i], cmap=matplotlib.cm.gray)
-        plt.xticks(np.array([]))
-        plt.yticks(np.array([]))
-        plt.tight_layout()
-    plt.show()
-#    compare_images(input_image[0,:,:,:],output_image[0][0,:,:,:])
-    exit()
-
-    
-    
-
 def network_model(X_train, y_train, epochs):
     
     model = Sequential()
-    #add normalization
     model.add(Lambda(lambda x: x /255.0 - 0.5 , input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((70,25),(0,0))))
-    # Conv Layer #0 (depth=3, kernel=1x1) - change color space
-    #model.add(Convolution2D(3, 1, 1, border_mode='same'))
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
     model.add(Convolution2D(48,3,3, subsample=(2,2), activation="relu"))
@@ -279,28 +207,23 @@ def network_model(X_train, y_train, epochs):
     model.add(Dense(10))
     model.add(Dropout(0.5))
     model.add(Dense(1))
-
-    #adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.01)
     model.compile(loss='mse', optimizer='adam')
-    #model.compile(loss='mse', optimizer=adam)
     history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=epochs)
     model.save('model.h5')
-#    plot_loss_histogram(history_object)
+    plot_loss_histogram(history_object)
     return model
-def augment_images(X_train, y_train):
-    new_X_train = X_train
-    new_y_train = y_train
-    return new_X_train, new_y_train
+
+
 #Main()  
+
 center_images, left_images, right_images, measurements = load_image_data()
 center_images, left_images, right_images, measurements = augment_data(center_images, left_images, right_images, measurements)
 
 X_train = np.array(center_images)
 y_train = np.array(measurements)
 plot_measurement_histogram(y_train)
-
 model = network_model(X_train, y_train,10)
-#visualize_model(X_train,model)
+
 
 
 
